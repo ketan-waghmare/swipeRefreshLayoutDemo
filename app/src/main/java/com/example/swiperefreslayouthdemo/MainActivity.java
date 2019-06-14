de7c3be1d6f18d11d2799e2758ce5d36f98a2387
unknown <brandzgarage@gmail.com>
2019-06-13 20:15:27 -0700

    message

---
+++ app/src/main/java/com/example/swiperefreslayouthdemo/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.swiperefreslayouthdemo;
+
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_hello_world)
+    TextView tvHelloWorld;
+    @BindView(R.id.swipeRefreshLayout)
+    SwipeRefreshLayout swipeRefreshLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        init();
+    }
+
+    private void init() {
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(false);
+                tvHelloWorld.setText("After Refresh Hello World");
+            }
+        });
+
+    }
+}
